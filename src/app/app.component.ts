1a7bc20e0adfda88e770da9cbf78abc22dbb9d38
Xounder <renanfellipe1@hotmail.com>
2024-09-12 19:18:35 -0300

    created project and necessary components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { BigCardComponent } from '../app/components/big-card/big-card.component'
+import { SmallCardComponent } from '../app/components/small-card/small-card.component'
+import { WelcomeCardComponent } from '../app/components/welcome-card/welcome-card.component'
+import { MenuBarComponent } from '../app/components/menu-bar/menu-bar.component'
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BigCardComponent, SmallCardComponent, WelcomeCardComponent, MenuBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
