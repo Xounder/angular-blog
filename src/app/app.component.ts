3eeb75800a1e454e2dd2e3a7dec8e3fe32de8460
Xounder <renanfellipe1@hotmail.com>
2024-09-12 21:49:44 -0300

    added routerLink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { HomeComponent } from '../app/pages/home/home.component'
+import { MenuBarComponent } from '../app/components/menu-bar/menu-bar.component'
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [
+    RouterOutlet,
+    HomeComponent,
+    MenuBarComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
